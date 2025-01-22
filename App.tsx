f833ec34991c1feb1646a7c4a67171a372d664cb
Jose Rojas <josejrojasc@hotmail.es>
2025-01-21 22:40:02 -0600

    Fix for Reanimated not working on web

--- App.tsx
+++ App.tsx
@@ -2,8 +2,17 @@ import "@expo/metro-runtime"
 import * as SplashScreen from "expo-splash-screen"
 import App from "@/app"
 import { useEffect } from "react"
+import { Platform } from "react-native"
 
 SplashScreen.preventAutoHideAsync()
+
+if (Platform.OS === "web") {
+  global._WORKLET = false
+  // @ts-expect-error
+  global._log = console.log
+  // @ts-expect-error
+  global._getAnimationTimestamp = () => performance.now()
+}
 
 function IgniteApp() {
   useEffect(() => {
